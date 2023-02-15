7b3092b67ef93dc039f6e79dfd84680116fa95bf
zuhriddin <zzayniddinov80@email.ru>
2023-02-15 14:15:14 +0300

    vazifa1

---
+++ index.js
@@ -0,0 +1,136 @@
+// let arr = [1,3,2,4,5,6,99,102];
+
+// let target = 5;
+
+// let elsort = arr.sort(function(a,b){return (a - b)});
+
+// let ersort = arr.sort(function(a,b){return (b - a)});
+
+// let qwer = elsort.indexOf(target)
+
+// console.log(elsort);
+// console.log(ersort);
+// console.log(qwer);
+
+// let arr = [2,3,8]
+
+// function newfuntion (num){
+//     console.log(num);
+//     for(let a = null )
+//     // if(arr[i] = 0 <= i++);
+//     // else if( )
+
+// }
+
+// newfuntion()
+
+// function newfuntion(...num) {
+//     let abc = num.reduce((a, b) => a + b, 0)
+//     console.log(abc);
+
+// }
+// newfuntion(5, 43)
+
+// let result = null
+// function newfuntion(...arr){
+//     for(let i = 0; i < arr.lengt; i++){
+//         result = result + arr[i]
+//     }
+//     console.log(result);
+// }
+
+// newfuntion(5, 43)
+
+// let result = null
+// function newFunction  (...arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         result = result + arr[i]
+//     }
+//     console.log(result);
+// }
+// newFunction(1,34,5)
+
+// let arr = ["olma","davlat","xalq","dasturchi","dasturlash","google","boolean"]
+// let arr2 = []
+
+// function
+
+// let arr = [ "olma","davlat","xalq","dasturchi","dasturlash","google","boolean"]
+// let result = []
+
+// function newFunction (...arr){
+//     for (let i = 0; i <= arr.length; i++) {
+//         if (arr[i] === result){
+//             result.push(arr[i])
+//         }
+//     }
+//     console.log(result.length > 0 ? 'soz bor': ' soz yoq');
+// }
+// newFunction(olma)
+
+// let arr = [1,23,43,56,6]
+
+// function funsiya(parametr){
+// let alsort = arr.sort(function(a,b){return a -b})
+// let result = alsort.indexOf(parametr)
+// console.log(alsort);
+// console.log(result);}
+
+
+// let obj1 = {a:4,b:1,c:9,d:5,f:7}
+// let obj2 = {e:3,g:2,h:8,j:6,l:10}
+// const arrow_function = (fuu) =>{
+// let obj3 = Object.assign(obj1,obj2)
+// let elvalues = Object.values(obj3)
+// let elsort = elvalues.sort(function(a,b){return a - b});
+// let forof = elsort.indexOf(fuu)
+// console.log(`o'sish tartibida ${forof} indexsida turibdi`);
+// // let ersort = elsort.reverse()
+// // console.log(obj3);
+// // console.log(elvalues);
+// console.log(elsort);
+// // console.log(ersort);
+// }
+
+// arrow_function(8)
+
+
+// let person = [
+//     {
+//         name: "Botir",
+//         age: 20,
+//     },
+//     {
+//         name: "Abdulla",
+//         age: 22,
+//     },
+//     {
+//         name: "Shukur",
+//         age: 18,
+//     },
+//     {
+//         name: "Jasur",
+//         age: 25,
+//     },
+// ];
+
+// const numbers = [2,5,8,6]
+
+// let elsort = person.sort(function(a,b){return a - b});
+
+// console.log(elsort);
+
+let menu = {
+    width: 150,
+    height: 600,
+    name: "menu",
+}
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+
+
